06356b2215d63a4f19abddeb564d6255d8375624
MilanVD <99069122@mydavinci.nl>
2021-09-14 13:36:33 +0200

    feestlunch.py

---
+++ feestlunch.py
@@ -0,0 +1,12 @@
+aantalCroissantjes=17
+prijsCroissantjes=0.39
+aantalStokbroden=2
+prijsStokbroden=2.78
+anntalKortingsbonnen=3
+totaalprijs= aantalCroissantjes*prijsCroissantjes + aantalStokbroden*prijsStokbroden - anntalKortingsbonnen
+
+
+print(" de feestlunch kost je bij de bakker " + str(totaalPrijs) + " voor de " + str(aantalCroissantjes)
+      + " croissantjes en de " + str(aantalStokbroden) + " stokbroden "
+      + " als de" + str(aantalkortingsbonnen) + "kortingsbonnen nog geldig zijn!")
+      
